2994375d1c793a60f3201e667853f1577ab72ffa
Reinhardt Rijna <69666626+ReinhardtR@users.noreply.github.com>
2023-05-28 20:41:59 +0200

    revet top level await, use IIFE

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,49 +5,51 @@ import { ReceiptParsingError } from "./lib/receipt-parsing-error";
 import FastifyMultipart from "@fastify/multipart";
 import FastifyCors from "@fastify/cors";
 
-const server = fastify({ logger: true });
+(async function () {
+  const server = fastify({ logger: true });
 
-await server.register(FastifyCors, {
-  origin: "*",
-  methods: ["POST"],
-});
-await server.register(FastifyMultipart);
+  await server.register(FastifyCors, {
+    origin: "*",
+    methods: ["POST"],
+  });
+  await server.register(FastifyMultipart);
 
-server.post("/", async (req, res) => {
-  const file = await req.file();
+  server.post("/", async (req, res) => {
+    const file = await req.file();
 
-  if (!file) {
-    res.status(400).send({ message: "No file provided" });
-    return;
-  }
-
-  if (file.mimetype !== "application/pdf") {
-    res.status(400).send({ message: "File is not a PDF" });
-    return;
-  }
-
-  const buffer = await file.toBuffer();
-  const pdf = await parsePdf(buffer);
-
-  try {
-    const receipt = parseReceiptContent(pdf);
-
-    res.send({ receipt });
-    return;
-  } catch (e) {
-    if (e instanceof ReceiptParsingError) {
-      res.status(400).send({ message: e.message });
+    if (!file) {
+      res.status(400).send({ message: "No file provided" });
       return;
     }
-  }
 
-  res.status(500).send({ message: "Internal server error" });
-});
+    if (file.mimetype !== "application/pdf") {
+      res.status(400).send({ message: "File is not a PDF" });
+      return;
+    }
 
-server.listen({ port: Number(process.env.PORT) || 8080 }, (err, address) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(`Server listening at ${address}`);
-});
+    const buffer = await file.toBuffer();
+    const pdf = await parsePdf(buffer);
+
+    try {
+      const receipt = parseReceiptContent(pdf);
+
+      res.send({ receipt });
+      return;
+    } catch (e) {
+      if (e instanceof ReceiptParsingError) {
+        res.status(400).send({ message: e.message });
+        return;
+      }
+    }
+
+    res.status(500).send({ message: "Internal server error" });
+  });
+
+  server.listen({ port: Number(process.env.PORT) || 8080 }, (err, address) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+  });
+})();
